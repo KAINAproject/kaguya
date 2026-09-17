2727c09c428c0bf584ba38482781c63b6cd20bdb
ojii3 <84656786+OJII3@users.noreply.github.com>
2026-09-18 08:30:45 +0900

    refactor(client): use vite-plugin-moonbit

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,20 +1,21 @@
 import { defineConfig } from "vite"
 import basicSsl from "@vitejs/plugin-basic-ssl"
+import moonbit from "vite-plugin-moonbit"
 import wasm from "vite-plugin-wasm"
 
-const kaguyaRuntime = new URL(
-  "./.moonbit-build/js/release/build/apps/client/src/src.js",
-  import.meta.url,
-).pathname
+const moonbitRoot = new URL("../..", import.meta.url).pathname
 
 export default defineConfig({
-  plugins: [basicSsl(), wasm()],
+  plugins: [
+    basicSsl(),
+    wasm(),
+    moonbit({
+      root: moonbitRoot,
+      target: "js",
+      mode: "release",
+    }),
+  ],
   build: {
     target: "esnext",
-  },
-  resolve: {
-    alias: {
-      "@kaguya/runtime": kaguyaRuntime,
-    },
   },
 })
